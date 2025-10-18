98df30acc52a2966861d18ab365efdc681c33a5f
Richardo Kaka Widjaja <105096180+Richardo05@users.noreply.github.com>
2025-10-18 14:03:59 +0700

    Update MoneyDetectionInterface.py

--- MoneyDetectionInterface.py
+++ MoneyDetectionInterface.py
@@ -3,7 +3,8 @@ from ultralytics.models.yolo.detect import DetectionPredictor
 import cv2
 
 
-model = YOLO("D:/uni/sem 3/AI/MoneyDetectionV2.pt") #ganti jadi path ke file .pt    
+model = YOLO("D:/***/***/***/MoneyDetectionV2.pt") #ganti jadi path ke file .pt    
 model.predict(source = "1", show = True, conf=0.5)
+
 
 # D:/uni/sem 3/AI/video-rupiah.mp4
